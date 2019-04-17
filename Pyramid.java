a3d7828604f5e699a1a64f9a73697ed36a1fb6e3
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-04-18 00:17:04 +0700

    Add pyramid generator

---
+++ Pyramid.java
@@ -0,0 +1,16 @@
+/**
+ * Pyramid
+ */
+public class Pyramid {
+    public static void main(String[] args) {
+        int rows = 15;
+        int k = 0;
+        for (int i = 1; i <= rows; i++, k = 0) {
+            for (int space = 0; space <= rows - i; space++)
+                System.out.print(" ");
+            while (k++ != i - 1)
+                System.out.print("* ");
+            System.out.println();
+        }
+    }
+}
