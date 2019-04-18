a351d4a8efbc1530cd52291666a201ac45615e57
Ignatius Bagus <ignatius.mbs@gmail.com>
2019-04-19 02:12:59 +0700

    Implement stringbuilder

--- Pyramid.java
+++ Pyramid.java
@@ -1,16 +1,15 @@
-/**
- * Pyramid
- */
 public class Pyramid {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
         int rows = 15;
         int k = 0;
         for (int i = 1; i <= rows; i++, k = 0) {
             for (int space = 0; space <= rows - i; space++)
-                System.out.print(" ");
+                sb.append(" ");
             while (k++ != i - 1)
-                System.out.print("* ");
-            System.out.println();
+                sb.append("* ");
+            System.out.println(sb.toString());
+            sb.setLength(0);
         }
     }
 }
