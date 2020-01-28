66023e31e8b481709a87c848e74039519e8cebf6
Ignatius Bagus <ignatius.mbs@gmail.com>
2020-01-28 11:38:18 +0700

    Add solutions for 0001, 0050, 0065, 0069, 0326

--- NewtonSqrt.py
+++
@@ -1,4 +0,0 @@
-estimate = userInput = int(input("Your number: "))
-while abs(estimate - userInput / estimate) > 1e-15 * estimate:
-    estimate = (userInput / estimate + estimate) / 2
-print(estimate)
