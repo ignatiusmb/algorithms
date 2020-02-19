7bdf73e985b40bf6967e3a66a8d4549e98ada1c1
Ignatius Bagus <ignatiusmbs@gmail.com>
2020-02-19 09:49:53 +0700

    Add defined shortcuts and replace fastscan with cin

--- template.cpp
+++ template.cpp
@@ -1,50 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Remove if not working with numbers
-void fastscan(int &number)
-{
-   bool negative = false;
-   register int c;
+// definiton optimization
+#define endl "\n"
 
-   number = 0;
-
-   c = getchar();
-   if (c == '-')
-   {
-      negative = true;
-      c = getchar();
-   }
-
-   for (; (c > 47 && c < 58); c = getchar())
-      number = number * 10 + c - 48;
-
-   if (negative)
-      number *= -1;
-}
+typedef long long ll;
+// data structures
+#define vi vector<int>
+#define vll vector<ll>
 
 int main()
 {
    // optimizations and rulesets
    ios_base::sync_with_stdio(false);
-   cin.tie(NULL);
+   cin.tie(0), cout.tie(0);
 
    // data structures and variables
-   int t, n, i, j, res;
-
-   fastscan(t);
-   // cin >> t;
+   int t, i, j, n, res;
+   cin >> t;
    for (i = 0; i < t; i++)
    {
-      fastscan(n);
-      // cin >> n;
-      int a[n];
-      for (j = 0; a[j]; j++)
+      cin >> n;
+      int a[n] = {0};
+      for (j = 0; j < n; j++)
       {
          // CODE starts here
       }
 
-      cout << res << "\n";
+      cout << res << endl;
+      // cout.flush();
    }
    return 0;
 }
