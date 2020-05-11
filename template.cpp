2d45d022b6c4d87719b28eda379de006de342c6d
Ignatius Bagus <ignatiusmbs@gmail.com>
2020-05-11 20:06:35 +0700

    Template -> v1.0.0

--- template.cpp
+++ template.cpp
@@ -1,34 +1,31 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// definiton optimization
-#define endl "\n"
-
 typedef long long ll;
-// data structures
-#define vi vector<int>
-#define vll vector<ll>
+typedef long double ld;
+typedef map<int, int> mii;
+typedef pair<int, int> ii;
+typedef pair<ld, ld> dd;
+typedef vector<int> vi;
+typedef vector<ii> vii;
+typedef vector<ll> vll;
+#define f first
+#define s second
 
-int main()
-{
-   // optimizations and rulesets
-   ios_base::sync_with_stdio(false);
-   cin.tie(0), cout.tie(0);
+#define endl "\n"
+#define rep(i, a, b) for (int i = a; i < (b); i++)
+#define sz(x) (int)(x).size()
 
-   // data structures and variables
-   int t, i, j, n, res;
-   cin >> t;
-   for (i = 0; i < t; i++)
-   {
-      cin >> n;
-      int a[n] = {0};
-      for (j = 0; j < n; j++)
-      {
-         // CODE starts here
-      }
+const ld eps = 1e-10;
 
-      cout << res << endl;
-      // cout.flush();
-   }
+int t;
+int main() {
+   cin.tie(0)->sync_with_stdio(0);
+   cin.exceptions(cin.failbit);
+   // if working with double
+   //cout << fixed << setprecision(9);
+
+   // cout << res << endl;
+   // cout.flush();
    return 0;
 }
